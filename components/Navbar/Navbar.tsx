bd7e9ed8058215c5adf189da9e86600e060b65b9
Maxim <144056146+TockePie@users.noreply.github.com>
2024-12-09 17:41:11 +0200

    new drawer

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -2,41 +2,24 @@
 
 import { useEffect, useState } from 'react'
 import { isMobile, isMobileOnly } from 'react-device-detect'
-import { useDisclosure } from '@dwarvesf/react-hooks'
 import {
-  Button,
-  Card,
-  CardBody,
   Chip,
-  Divider,
   Image,
   Navbar,
   NavbarBrand,
   NavbarContent
 } from '@nextui-org/react'
-import { Menu } from 'lucide-react'
-import dynamic from 'next/dynamic'
 import Link from 'next/link'
-import { useRouter } from 'next/navigation'
 
 import { NAVBAR_TEXTS } from '@/common/constants/texts'
 import useGroup from '@/common/providers/group'
-import { Sidebar } from '@/components/ui/Sidebar'
 
+import DrawerComp from './components/Drawer'
 import GroupDropdownMenu from './components/DropdownMenu'
 
-const Settings = dynamic(() => import('../Settings/Settings'), { ssr: false })
-
 const NavbarComponent = () => {
-  const { isOpen, onOpen, onClose } = useDisclosure()
   const [isClient, setIsClient] = useState(false)
-  const router = useRouter()
   const { logo } = useGroup()
-
-  const handleHelpPress = () => {
-    router.push('/help')
-    onClose()
-  }
 
   useEffect(() => setIsClient(true), [])
 
@@ -44,41 +27,21 @@ const NavbarComponent = () => {
   const shouldHideOnScroll = isClient ? isMobileOnly : false
 
   return (
-    <>
-      <Navbar isBordered={isBordered} shouldHideOnScroll={shouldHideOnScroll}>
-        <NavbarBrand>
-          <Link href="/">
-            <Image src={logo} alt="Logo" width={50} />
-          </Link>
-          <b className="hidden p-3 text-2xl md:inline">
-            {NAVBAR_TEXTS.schedule}
-          </b>
-          <Chip color="warning" size="md" className="mx-2">
-            Beta
-          </Chip>
-        </NavbarBrand>
-        <NavbarContent justify="end">
-          <GroupDropdownMenu />
-          <Button isIconOnly color="primary" variant="faded" onClick={onOpen}>
-            <Menu />
-          </Button>
-        </NavbarContent>
-      </Navbar>
-
-      <Sidebar onClose={onClose} isOpen={isOpen}>
-        <Settings />
-        <Divider className="my-4" />
-        <Card
-          isPressable
-          onPress={handleHelpPress}
-          className="h-24 w-full cursor-pointer"
-        >
-          <CardBody className="justify-center text-center text-xl">
-            Допомога
-          </CardBody>
-        </Card>
-      </Sidebar>
-    </>
+    <Navbar isBordered={isBordered} shouldHideOnScroll={shouldHideOnScroll}>
+      <NavbarBrand>
+        <Link href="/">
+          <Image src={logo} alt="Logo" width={50} />
+        </Link>
+        <b className="hidden p-3 text-2xl md:inline">{NAVBAR_TEXTS.schedule}</b>
+        <Chip color="warning" size="md" className="mx-2">
+          Beta
+        </Chip>
+      </NavbarBrand>
+      <NavbarContent justify="end">
+        <GroupDropdownMenu />
+        <DrawerComp />
+      </NavbarContent>
+    </Navbar>
   )
 }
 
---
+++ components/Navbar/components/Drawer.tsx
@@ -0,0 +1,110 @@
+'use client'
+
+import { useEffect, useState } from 'react'
+import { isMobileOnly } from 'react-device-detect'
+import {
+  Button,
+  Card,
+  CardBody,
+  Chip,
+  Divider,
+  Drawer,
+  DrawerBody,
+  DrawerContent,
+  DrawerFooter,
+  DrawerHeader,
+  Select,
+  SelectItem,
+  Switch,
+  useDisclosure
+} from '@nextui-org/react'
+import { LifeBuoy, Menu } from 'lucide-react'
+import { useRouter } from 'next/navigation'
+
+const DrawerComp = () => {
+  const { isOpen, onOpen, onOpenChange } = useDisclosure()
+  const [isPwa, setIsPwa] = useState(false)
+  const router = useRouter()
+
+  useEffect(() => {
+    const storedValue = localStorage.getItem('isPwa')
+    setIsPwa(storedValue === 'true')
+  }, [])
+
+  const handlePwa = (checked: boolean): void => {
+    setIsPwa(checked)
+    localStorage.setItem('enabledPwaZoom', checked.toString())
+  }
+
+  return (
+    <>
+      <Button isIconOnly color="primary" variant="faded" onClick={onOpen}>
+        <Menu />
+      </Button>
+      <Drawer
+        size="sm"
+        isOpen={isOpen}
+        onOpenChange={onOpenChange}
+        hideCloseButton
+      >
+        <DrawerContent>
+          {(onClose) => (
+            <>
+              <DrawerHeader className="text-2xl font-bold">
+                Налаштування
+              </DrawerHeader>
+              <DrawerBody>
+                <Card
+                  isPressable
+                  onPress={() => {
+                    router.push('/help')
+                    onClose()
+                  }}
+                  className="cursor-pointer"
+                >
+                  <CardBody className="flex flex-row gap-x-3">
+                    <LifeBuoy strokeWidth={1.5} />
+                    <p className="font-semibold">Допомога</p>
+                  </CardBody>
+                </Card>
+
+                <Divider className="my-3" />
+
+                <Select
+                  isDisabled
+                  label="Вибірковий предмет"
+                  placeholder="Поки що недоступно"
+                >
+                  <SelectItem key="math" value="math">
+                    Математика
+                  </SelectItem>
+                </Select>
+                {!isMobileOnly && (
+                  <Switch isSelected={isPwa} onValueChange={handlePwa}>
+                    Відкривати посилання ZOOM у браузері{' '}
+                    <Chip size="sm" color="danger">
+                      Unstable
+                    </Chip>
+                  </Switch>
+                )}
+              </DrawerBody>
+              <DrawerFooter>
+                <Button
+                  size="lg"
+                  color="primary"
+                  variant="faded"
+                  onClick={onClose}
+                  className="w-full"
+                >
+                  Закрити
+                </Button>
+              </DrawerFooter>
+            </>
+          )}
+        </DrawerContent>
+      </Drawer>
+    </>
+  )
+}
+
+export default DrawerComp
